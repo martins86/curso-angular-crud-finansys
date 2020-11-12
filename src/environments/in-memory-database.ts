68f61f5223cc0d5a59f1e36b5d2881964a8ac896
Thiago Martins de Freitas <martins86@gmail.com>
2020-11-12 19:55:16 -0300

    move in-memory-database

--- src/environments/in-memory-database.ts
+++ src/environments/in-memory-database.ts
@@ -1,8 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { CategoryModel } from './../app/pages/categories/shared/models/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
+    const categories: CategoryModel[] = [
       {id: 1, name: 'Moradia', description: 'contas de casa'},
       {id: 2, name: 'Carro', description: 'contas do carro'}
     ];
