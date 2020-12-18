befa55b4bd1e68c9e90b8fcdc25d3adf6f80098e
martins86 <martins86@gmail.com>
2020-12-18 23:46:46 +0000

    improving category list with base resource list

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,9 @@
+import { Component } from '@angular/core';
+
+import { BaseResourceListDirective } from 'src/app/shared/directives/base-resource-list/base-resource-list.directive';
+
 import { CategoryModel } from './../shared/models/category.model';
-import { Component, OnInit } from '@angular/core';
+
 import { CategoryService } from '../shared/services/category.service';
 
 @Component({
@@ -7,33 +11,9 @@ import { CategoryService } from '../shared/services/category.service';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListDirective<CategoryModel> {
 
-  categories: CategoryModel[] = [];
-
-  constructor(private serviceCategory: CategoryService) { }
-
-  ngOnInit(): void {
-    this.getAllCategories();
-  }
-
-  getAllCategories(): any {
-    this.serviceCategory.getAll().subscribe(
-      result => this.categories = result,
-      error => alert('Erro ao carregar as Categorias, tente novamente.')
-    );
-  }
-
-  deleteCategory(category: CategoryModel): any {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if (mustDelete) {
-      this.serviceCategory
-        .delete(category.id)
-        .subscribe(
-          () => this.categories = this.categories.filter(element => element !== category),
-          () => alert('Erro ao tentar excluir.')
-        );
-    }
+  constructor(private categoryService: CategoryService) {
+    super(categoryService);
   }
 }
