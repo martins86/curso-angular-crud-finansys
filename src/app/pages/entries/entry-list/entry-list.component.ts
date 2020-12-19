b98230c40084ca03900f2b3389bd0f896ad8de60
martins86 <martins86@gmail.com>
2020-12-19 00:03:00 +0000

    refactor entry list with base resource list

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,5 +1,9 @@
+import { Component } from '@angular/core';
+
+import { BaseResourceListDirective } from 'src/app/shared/directives/base-resource-list/base-resource-list.directive';
+
 import { EntryModel } from './../shared/models/entry.model';
-import { Component, OnInit } from '@angular/core';
+
 import { EntryService } from '../shared/services/entry.service';
 
 @Component({
@@ -7,33 +11,9 @@ import { EntryService } from '../shared/services/entry.service';
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
+export class EntryListComponent extends BaseResourceListDirective<EntryModel> {
 
-  entries: EntryModel[] = [];
-
-  constructor(private serviceEntry: EntryService) { }
-
-  ngOnInit(): void {
-    this.getAllCategories();
-  }
-
-  getAllCategories(): any {
-    this.serviceEntry.getAll().subscribe(
-      result => this.entries = result.sort((a, b) => b.id - a.id),
-      error => alert('Erro ao carregar os Lançamentos, tente novamente.')
-    );
-  }
-
-  deleteEntry(entry: EntryModel): any {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if (mustDelete) {
-      this.serviceEntry
-        .delete(entry.id)
-        .subscribe(
-          () => this.entries = this.entries.filter(element => element !== entry),
-          () => alert('Erro ao tentar excluir.')
-        );
-    }
+  constructor(private entryService: EntryService) {
+    super(entryService);
   }
 }
