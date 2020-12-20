90854dc2a460eafad4629b3639fa3d919e0aca68
martins86 <martins86@gmail.com>
2020-12-20 17:05:11 +0000

    import and export router module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+
 
 
 @NgModule({
   declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    RouterModule
   ],
 })
 export class SharedModule { }
