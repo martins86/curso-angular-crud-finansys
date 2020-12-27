624fe17419f43c2c5dc4d8088488fb44a44cfb93
martins86 <martins86@gmail.com>
2020-12-27 16:29:24 +0000

    create and extrating navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule,
     CommonModule
   ],
   exports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NavbarComponent,
   ]
 })
 export class CoreModule { }
