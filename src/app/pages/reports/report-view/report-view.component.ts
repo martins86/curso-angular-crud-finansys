68e24ef80ff0de3d3ff9211154a10fe6e04826fc
martins86 <martins86@gmail.com>
2020-12-28 01:08:09 +0000

    adding reports template

--- src/app/pages/reports/report-view/report-view.component.ts
+++ src/app/pages/reports/report-view/report-view.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-view.component.css']
 })
 export class ReportViewComponent implements OnInit {
+  months = [
+    {value: 1, text: 'Janeiro'},
+    {value: 2, text: 'Fevereiro'},
+    {value: 3, text: 'Março'},
+    {value: 4, text: 'Abril'},
+    {value: 5, text: 'Maio'},
+    {value: 6, text: 'Junho'},
+    {value: 7, text: 'Julho'},
+    {value: 8, text: 'Agosto'},
+    {value: 9, text: 'Setembro'},
+    {value: 10, text: 'Outubro'},
+    {value: 11, text: 'Novembro'},
+    {value: 12, text: 'Dezembro'}
+  ];
+
+  years = [
+    {value: 2015, text: '2015'},
+    {value: 2016, text: '2016'},
+    {value: 2017, text: '2017'},
+    {value: 2018, text: '2018'},
+    {value: 2019, text: '2019'},
+    {value: 2020, text: '2020'},
+    {value: 2021, text: '2021'}
+  ];
 
   constructor() { }
 
