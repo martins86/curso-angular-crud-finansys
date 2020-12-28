29f20d2921a30efc4aaaf5b2f99fc72fcec78d34
martins86 <martins86@gmail.com>
2020-12-28 01:17:02 +0000

    improving page header

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.css']
 })
-export class PageHeaderComponent implements OnInit {
+export class PageHeaderComponent {
 
   @Input('page-title') pageTitle: string;
+  @Input('show-button') showButton: boolean = true;
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonText: string;
   @Input('button-link') buttonLink: string;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
